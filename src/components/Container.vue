b8aa8b38592cd0e45f612c80e547ef4aa2496a4c
cornacchia <pozzan.gabriele@gmail.com>
2018-01-16 20:51:14 +0100

    Add search box and home button

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -1,10 +1,26 @@
 <template>
   <q-layout>
     <q-toolbar slot="header" color="green">
+      <q-btn flat @click="goHome">
+        <q-icon name="home"
+        />
+      </q-btn>
       <q-toolbar-title>
         {{ title }}
       </q-toolbar-title>
     </q-toolbar>
+    <q-input
+    id="searchBar"
+    placeholder="Cerca..."
+    v-model="search"
+    :after="[
+      {
+        icon: 'arrow_forward',
+        content: true,
+        handler: searchPlant
+      }
+    ]"
+    />
 
     <div id="main" class="column scroll overflow-hidden">
       <router-view />
@@ -13,22 +29,40 @@
 </template>
 
 <script>
-import { QLayout, QToolbar, QToolbarTitle } from 'quasar'
+import { QLayout, QToolbar, QToolbarTitle, QInput, QIcon, QBtn } from 'quasar'
+import router from '../router'
 
 export default {
   components: {
     QLayout,
     QToolbar,
-    QToolbarTitle
+    QToolbarTitle,
+    QInput,
+    QIcon,
+    QBtn
   },
   data () {
     return {
-      title: this.siteData.title
+      title: this.siteData.title,
+      search: ''
+    }
+  },
+  methods: {
+    searchPlant () {
+      let searchPlant = this.siteData.search[this.search]
+      if (searchPlant) {
+        router.replace({ path: '/plant', query: { name: searchPlant } })
+        return window.location.reload(true)
+      }
+    },
+    goHome () {
+      router.replace({path: '/home'})
     }
   }
 }
 </script>
 
-<style>
-
+<style lang="stylus">
+  #searchBar
+    margin 15px
 </style>
